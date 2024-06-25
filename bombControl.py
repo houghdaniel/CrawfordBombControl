cbe82119e340f5afd0c580c1f873335484489362
houghdaniel <hough.daniel.s@gmail.com>
2024-06-25 16:27:56 -0500

    Added UNTESTED fill and vent logic

--- bombControl.py
+++ bombControl.py
@@ -13,15 +13,17 @@ class BombControl:
 
     def __init__(self):
         self.handle = ljm.openS("T4", "ANY", "ANY")
-        self.fill_valve = ""
-        self.exhaust_valve = ""
+        self.fill_valve = "TDAC7"
+        self.exhaust_valve = "DAC1"
         self.ignite = ""
+        self.open = 0
+        self.close = 5
 
     def fill(self):
         # fill valve open exhaust valve closed, while loop until desired pressure met, updating measured pressure as we go
-        ljm.eWriteName(self.handle, "DAC1", 5)
-        time.sleep(0.25)
-        ljm.eWriteName(self.handle, "DAC1", 0)
+        ljm.eWriteName(self.handle, self.fill_valve, self.open)
+        ljm.eWriteName(self.handle, self.exhaust_valve, self.close)
+
 
     def vent(self):
         # write
--- main.py
+++ main.py
@@ -1,42 +1,68 @@
 # GUI
 import sys
 import os
-from bombControl import BombControl
+import time
 import threading
+
+#from bombControl import BombControl
+from labjack import ljm
+
 from PySide6.QtCore import QSize, Qt
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 from PySide6 import QtGui
 from PySide6.QtUiTools import QUiLoader
 
+class BombControl:
 
-#class MainWindow(QMainWindow):
-#    def __init__(self):
-#        super().__init__()
-#        self.bomb = BombControl()
-#        self.setWindowTitle("Crawford Bomb Control")
-#
-#        button = QPushButton("Start")
-#        button.setFixedSize(QSize(100,50))
-#        button.clicked.connect(self.button_pressed)
-#
-#        self.setMinimumSize(QSize(800,600))
-#
-#        self.setCentralWidget(button)
-#        
-#    
-#    def button_pressed(self):
-#        print("Clicked")
-#        t1 = threading.Thread(target=self.bomb.fill())
-#        t1.start()
+    def __init__(self):
+        self.handle = ljm.openS("T4", "ANY", "ANY")
+        self.fill_valve = "TDAC7"
+        self.exhaust_valve = "DAC1"
+        self.ignite = ""
+        self.transducer = "AIN3"
+
+        self.open = 0
+        self.close = 5
+        self.set_pressure = 0
+        self.measured_pressure = ljm.eReadName(self.handle, self.transducer) * 600
+
+        window.measuredPressureField.setText(self.measured_pressure)
+
+    def fill(self):
+        ljm.eWriteName(self.handle, self.fill_valve, self.open) # Open fill valve
+        ljm.eWriteName(self.handle, self.exhaust_valve, self.close) # Close exhaust valve
+
+        self.set_pressure = window.setPressureField.text() # Get set pressure
+        
+        # While measured pressure is below set pressure, keep checking measured pressure and updating field
+        while self.set_pressure > self.measured_pressure:
+            self.measured_pressure = ljm.eReadName(self.handle, self.transducer) * 600
+            window.measuredPressureField.setText(self.measured_pressure)
+        ljm.eWriteName(self.handle, self.fill_valve, self.close) # Close fill valve
+
+
+
+
+    def vent(self):
+        # First, end data recording
+        ljm.eWriteName(self.handle, self.fill_valve, self.close) # Close fill valve
+        ljm.eWriteName(self.handle, self.exhaust_valve, self.open) # Open exhaust valve
+        
+
+    def ignition(self):
+        # Begin data recording and trigger pulse for ignition
+        pass
+        
+
 
 loader = QUiLoader()
 bomb = BombControl()
 
 def mainwindow_setup(w):
     w.setWindowTitle("Crawford Bomb Control")
-    w.fillButton.clicked.connect(fill_button_pressed) #stop DAC1
-    w.ventButton.clicked.connect(vent_button_pressed) #release DAC0
-    w.ignitionButton.clicked.connect(ignition_button_pressed) #run TDAC7
+    w.fillButton.clicked.connect(fill_button_pressed) 
+    w.ventButton.clicked.connect(vent_button_pressed) 
+    w.ignitionButton.clicked.connect(ignition_button_pressed)
 
 def fill_button_pressed():
     print("fill pressed\n")
@@ -60,4 +86,8 @@ window = loader.load("mainwindow.ui", None)
 mainwindow_setup(window)
 window.show()
 app.exec()
+
+
+
+
 
