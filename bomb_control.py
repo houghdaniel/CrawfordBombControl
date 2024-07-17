83aaa70cb095ac34503d77a37007e991bf0aa348
Daniel Hough <hough.daniel.s@gmail.com>
2024-07-17 15:38:02 -0500

    Moved DAQ IO ports to settings file, and made saving function replace forbidden filename characters with '_'

--- bomb_control.py
+++ bomb_control.py
@@ -8,16 +8,16 @@ class BombControl:
 
     def __init__(self, window):
         self.handle = ljm.openS("T4", "ANY", "ANY")
-        self.fill_valve = "TDAC7"
-        self.exhaust_valve = "DAC1"
-        self.ignite = "TDAC6"
-        self.transducer = "AIN3"
+        self.fill_valve = s.fill_valve
+        self.exhaust_valve = s.exhaust_valve
+        self.ignite = s.ignite
+        self.transducer = s.transducer
         self.window = window
 
         self.open = 0
         self.close = 5
         self.set_pressure = 0
-        self.measured_pressure = ljm.eReadName(self.handle, self.transducer) * 600 - s.pressure_offset
+        self.measured_pressure = ljm.eReadName(self.handle, self.transducer) * s.v_mult - s.pressure_offset
 
         window.measuredPressureField.setText("%.2f"%(self.measured_pressure))
 
@@ -35,14 +35,21 @@ class BombControl:
         
         # While measured pressure is below set pressure, keep checking measured pressure and updating field
         while self.set_pressure > self.measured_pressure:
-            self.measured_pressure = ljm.eReadName(self.handle, self.transducer) * 600 - s.pressure_offset
+            self.measured_pressure = ljm.eReadName(self.handle, self.transducer) * s.v_mult - s.pressure_offset
             self.window.measuredPressureField.setText("%.2f"%(self.measured_pressure))
         ljm.eWriteName(self.handle, self.fill_valve, self.close) # Close fill valve
 
     def vent(self):
         # First, end data recording and log to file
         s.running = False
-        save_file = self.window.dataSaveFolderField.text() + self.window.sampleIDField.text() + ".csv"
+        sample_ID = self.window.sampleIDField.text()
+
+        # Replace special characters in sample ID filename with '_'
+        for symbol in s.forbidden:
+            if symbol in sample_ID:
+                sample_ID = sample_ID.replace(symbol, '_')
+
+        save_file = self.window.dataSaveFolderField.text() + sample_ID + ".csv"
 
         with open(save_file, mode='w', newline='') as file:
             writer = csv.writer(file)
@@ -50,6 +57,7 @@ class BombControl:
             for i in range(len(s.times)):
                 writer.writerow([s.times[i],s.voltages[i],s.pressures[i]])
         print("Data written to " + save_file)
+        ljm.eWriteName(self.handle, self.ignite, self.open) # Open relay to ignition circuit
 
         # After data is saved, clear arrays to be able to start new experiment without restarting program
         s.times.clear()
@@ -63,7 +71,7 @@ class BombControl:
         # Begin data recording
         s.running = True
         # Close relay for ignition
-        #ljm.eWriteName(self.handle, self.ignite, self.close)
+        ljm.eWriteName(self.handle, self.ignite, self.close)
 
         # TODO: add another pulse for triggering other equipment
 
@@ -72,13 +80,13 @@ class BombControl:
         while s.running:
             t = time.time() - start_time
             v = ljm.eReadName(self.handle, self.transducer)
-            p = v * 600 - s.pressure_offset
+            p = v * s.v_mult - s.pressure_offset
 
             s.times.append(t)
             s.voltages.append(v)
             s.pressures.append(p)
 
-            time.sleep(1/100)
+            time.sleep(1/s.hz)
 
     def purge(self):
         ljm.eWriteName(self.handle, self.exhaust_valve, self.open)
@@ -88,5 +96,5 @@ class BombControl:
 
 
     def update(self):
-        p = ljm.eReadName(self.handle, self.transducer) * 600 - s.pressure_offset
+        p = ljm.eReadName(self.handle, self.transducer) * s.v_mult - s.pressure_offset
         self.window.measuredPressureField.setText("%.2f"%(p))
--- settings.py
+++ settings.py
@@ -1,6 +1,19 @@
+v_mult = 600
 pressure_offset = 8
 
+hz = 100
+
+# DAC IO ports
+fill_valve = "TDAC7"
+exhaust_valve = "DAC1"
+ignite = "DAC0"
+transducer = "AIN3"
+
+# Lists for data
 times = []
 voltages = []
 pressures = []
 running = False
+
+# List of forbidden characters for filename
+forbidden = ['*', '?', '<', '>', ':', '"', "'", '\\', '/', '|']
